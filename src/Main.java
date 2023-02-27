1caf712a1d1da6537bdf4b1c697cbf513c0111b8
youngLuke26 <YoungLT22@gcc.edu>
2023-02-27 10:46:46 -0500

    Scanner added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        System.out.println("Hello " + console.next());
 
     }
 }
